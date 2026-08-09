892ea24f3ccac7a07c6e94952be892cea3ece239
Ashmit Sharma <warloard2005@gmail.com>
2026-08-09 13:38:18 +0530

    Diamond printing code

--- __JAVA__/INTRODUCTION.java
+++ __JAVA__/INTRODUCTION.java
@@ -6,21 +6,37 @@ import java.util.Scanner;
  * INTRODUCTION
  */
 public class INTRODUCTION {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the character you want to enter :");
 
-        char specialt = sc.next().charAt(0);
-
-        if(Character.isLetter(specialt)){
-            System.out.println("an aplhabet");
+        System.out.println("enter the number of rows :");
+        int rows = sc.nextInt();
+        
+        
+        // dimond printing
+        Diamond(rows);
+    }
+    public static void Diamond(int rows){
+        for(int i =0;i<rows;i++){
+            for(int j=0;j<rows-1-i;j++){
+                System.out.print(" ");
+            }
+            for(int j =0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            
+        System.out.println();
         }
-        else if(Character.isDigit(specialt)){
-            System.out.println("is the digit");
-        }
-        else{
-            System.out.println("special character");
+        for(int i =rows-2;i>=0;i--){
+            for(int j=0;j<rows-1-i;j++){
+                System.out.print(" ");
+            }
+            for(int j =0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            
+        System.out.println();
         }
     }
-    
 }
